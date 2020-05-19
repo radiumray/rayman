be0aa43825204a9fb9396c92f9198a178598706d
zhanglei <2403613031@qq.com>
2020-05-19 17:10:32 +0800

    add docker mysql

--- dbUsersModel/admin.py
+++ dbUsersModel/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from dbUsersModel.models import userInfo, Contact, Tag
 
 # Register your models here.
+
+admin.site.register([userInfo, Contact, Tag])
+
+
+# class ContactAdmin(admin.ModelAdmin):
+#     fields = ('name', 'email')
+
+# admin.site.register(Contact, ContactAdmin)
+# admin.site.register([userInfo, Tag])
--- dbUsersModel/models.py
+++ dbUsersModel/models.py
@@ -5,3 +5,17 @@ from django.db import models
 class userInfo(models.Model):
 	name = models.CharField(max_length=20)
 	birthday = models.DateField()
+
+
+class Contact(models.Model):
+    name   = models.CharField(max_length=200)
+    age    = models.IntegerField(default=0)
+    email  = models.EmailField()
+    def __unicode__(self):
+        return self.name
+
+class Tag(models.Model):
+	contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+	name    = models.CharField(max_length=50)
+	def __unicode__(self):
+		return self.name
