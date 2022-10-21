350772d7875a29ac5a0e5cb412573d69ee904cf4
张雷 <radiumray9@gmail.com>
2022-10-21 14:08:13 +0800

    Create 数据集追加脚本.py

---
+++ 数据集追加脚本.py
@@ -0,0 +1,19 @@
+import os
+dataDir = 'trafficLight'
+prefix = 'traffic_light_'
+
+
+path = os.path.abspath('.')
+lst = os.listdir(dataDir)
+path = path + '/' + dataDir
+print(lst)
+for name in lst:
+    newName = prefix+ name
+    path_name = path + '/'+ name
+    path_newName = path + '/' + newName
+    is_exists = os.path.exists(path_name)
+    if is_exists is False:
+        print('file path is error')
+        break
+    os.rename(path_name, path_newName)
+    print(path_newName)
